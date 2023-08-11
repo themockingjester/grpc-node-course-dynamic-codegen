ab3d07740c796db1b1fc8aff0e4d8c80de040c63
themockingjester <yashmathur123123@gmail.com>
2023-08-12 00:04:24 +0530

    added demo calls for server streamingg and client streaming

--- client/client.js
+++ client/client.js
@@ -7,44 +7,106 @@ const grpc = require('grpc')
 
 const greetProtoPath = path.join(__dirname, "..", "protos", "greet.proto")
 const greetProtoDefinition = protoLoader.loadSync(greetProtoPath, {
-     keepCase: true,
-      longs: String,
-      enums: String,
-      defaults: true,
-      oneofs: true
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
 });
 
 const greetPackageDefinition = grpc.loadPackageDefinition(greetProtoDefinition).greet
 
 const client = new greetPackageDefinition.GreetService("localhost:50051",
-        grpc.credentials.createInsecure()
+    grpc.credentials.createInsecure()
 )
 
-function callGreetings() {
+//  For unary server demo
+function callGreeting() {
     var request = {
-         greeting: {
-              first_name: "Jerry",
-              last_name: "Tom"
-         }
+        greeting: {
+            first_name: "Jerry",
+            last_name: "Tom"
+        }
     }
-     
-
     client.greet(request, (error, response) => {
-          if(!error) {
-              console.log("Greeting Response: ", response.result);
-              
+        if (!error) {
+            console.log("Greeting Response: ", response.result);
 
-          }else {
-              console.error(error)
-              
-               
-          }
+
+        } else {
+            console.error(error)
+
+
+        }
     })
+}
+
+// For server streaming server demo
+function callMultipleGreeting() {
+    var request = {
+        greeting: {
+            first_name: "Jerry",
+            last_name: "Tom"
+        }
+    }
+    let call = client.sendMultipleGreetings(request, () => {
+    })
+
+    call.on('data', (response) => {
+        console.log("Client streaming response: " + response.result);
+    })
+    call.on('status', (status) => {
+        console.log(status);
+    })
+    call.on('error', (error) => {
+        console.error("Client streaming error: " + error);
+    })
+    call.on('end', (error) => {
+        console.error("Streamingg Ended");
+    })
+}
+
+// For client streaming demo
+function sendMultipleGreeting() {
+    let count = 0
+    var request = {
+        greeting: {
+            first_name: "Jerry",
+            last_name: "Tom"
+        }
+    }
+    let call = client.ReceiveMultipleGreetings(request, (error, response) => {
+        if (!error) {
+            console.log("Response: " + response.result);
+        } else {
+            console.log("Error: " + error);
+        }
+    })
+    let intervalId = setInterval(() => {
+        var request = {
+            greeting: {
+                first_name: "Jerry",
+                last_name: "Tom"
+            }
+        }
+        call.write(request)
+
+        if (count > 10) {
+            clearInterval(intervalId)
+            call.end()
+        }
+        count++
+    }, 500)
+
+
+
+    
 }
 
 
 function main() {
-    callGreetings()
-
+    // callGreeting()
+    // callMultipleGreeting()
+    sendMultipleGreeting()
 }
 main()
--- server/index.js
+++ server/index.js
@@ -7,35 +7,82 @@ const grpc = require('grpc')
 
 const greetProtoPath = path.join(__dirname, "..", "protos", "greet.proto")
 const greetProtoDefinition = protoLoader.loadSync(greetProtoPath, {
-     keepCase: true,
-      longs: String,
-      enums: String,
-      defaults: true,
-      oneofs: true
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
 });
 
 const greetPackageDefinition = grpc.loadPackageDefinition(greetProtoDefinition).greet
 
+
+// For unary server demo (similar to rest api)
 function greet(call, callback) {
-    var firstName = call.request.greeting.first_name;
+    let firstName = call.request.greeting.first_name;
     var lastName = call.request.greeting.last_name;
 
-    callback(null, {result: "Hello " + firstName + " " + lastName})
+    callback(null, { result: "Hello " + firstName + " " + lastName })
 
 }
 
+
+// For server streaming demo
+function greetMany(call, callback) {
+    let firstName = call.request.greeting.first_name;
+    let lastName = call.request.greeting.last_name;
+    let count = 0
+
+    let interval = setInterval(() => {
+        let greetManyTimesResponse = greetPackageDefinition.SendManyGreetingsResponse
+        greetManyTimesResponse.result = "Hello " + firstName + " " + lastName
+        call.write(greetManyTimesResponse)
+
+        if (count > 10) {
+            clearInterval(interval)
+            // ending stream
+            call.end()
+        }
+        count += 1
+    }, 1000)
+
+
+
+
+}
+
+
+function receiveManyGreet(call, callback) {
+
+    call.on('data', (request) => {
+        let firstName = request.greeting.first_name;
+        let lastName = request.greeting.last_name;
+        console.log("Received : " + firstName + ", " + lastName);
+    })
+    call.on('error', (error) => {
+        console.error("Server streaming error: " + error);
+    })
+    call.on('end', () => {
+        console.error("Streaming Ended");
+        let serverResponse = greetPackageDefinition.GreetResponse
+        serverResponse.result = "Thanks For sending data "
+        callback(null,serverResponse)
+    })
+}
 function main() {
     const server = new grpc.Server()
 
     server.addService(greetPackageDefinition.GreetService.service, {
-         greet: greet
+        greet: greet,
+        sendMultipleGreetings: greetMany,
+        receiveMultipleGreetings:receiveManyGreet
     })
 
     server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure())
     server.start()
     console.log("Server Runnig at http://127.0.0.1:50051")
-    
-    
+
+
 
 }
 main()
